b3c93aa1038e363e11782e6c01b787fb0af5818e
DevExpressExampleBot <fakeEmail>
2020-06-16 12:18:48 +0300

    Source auto update [skip ci]

---
+++ VB/RealTimeChartUpdates/Form1.Designer.vb
@@ -0,0 +1,60 @@
+﻿Namespace RealTimeChartUpdates
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
+			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' chartControl1
+			' 
+			Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.chartControl1.Legend.Name = "Default Legend"
+			Me.chartControl1.Location = New System.Drawing.Point(0, 0)
+			Me.chartControl1.Name = "chartControl1"
+			Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series(){}
+			Me.chartControl1.Size = New System.Drawing.Size(1267, 644)
+			Me.chartControl1.TabIndex = 0
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(8F, 16F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(1267, 644)
+			Me.Controls.Add(Me.chartControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
+'ORIGINAL LINE: this.Load += new System.EventHandler(this.Form1_Load);
+			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private chartControl1 As DevExpress.XtraCharts.ChartControl
+	End Class
+End Namespace
+
---
+++ VB/RealTimeChartUpdates/Program.vb
@@ -0,0 +1,20 @@
+﻿Imports DevExpress.XtraEditors
+Imports System
+Imports System.Windows.Forms
+
+Namespace RealTimeChartUpdates
+	Friend Module Program
+
+		<STAThread>
+		Sub Main()
+			WindowsFormsSettings.ForceDirectXPaint()
+			WindowsFormsSettings.SetDPIAware()
+			WindowsFormsSettings.AllowDpiScale = True
+			WindowsFormsSettings.AllowAutoScale = DevExpress.Utils.DefaultBoolean.True
+			Application.EnableVisualStyles()
+			Application.SetCompatibleTextRenderingDefault(False)
+			Application.Run(New Form1())
+
+		End Sub
+	End Module
+End Namespace
